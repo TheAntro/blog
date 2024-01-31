d8fef73bdb50eca2772a4a9a2355af17e2a48fa2
Valtteri Ranta <waltherbeach@gmail.com>
2024-01-31 15:10:11 +0200

    Setup article publishing with Storyblok
    Also a link to the first blog post added to the home page

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,6 +15,7 @@ export default defineConfig({
       components: {
         page: "storyblok/Page",
         teaser: "storyblok/Teaser",
+        article: "storyblok/Article",
       },
     }),
   ],
